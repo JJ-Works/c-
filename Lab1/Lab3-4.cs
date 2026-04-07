2687eec5df3a6f62a423add0d6381464a0afa017
Janarjan <janarjanrb@gmail.com>
2026-04-07 14:14:23 +0545

    new programs added.

--- Lab1/Lab3-4.cs
+++ Lab1/Lab3-4.cs
@@ -2,9 +2,48 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Lab1
-{
+
     internal class Lab3_4
     {
+        public void addColumn()
+        {
+            Console.WriteLine("Write a C# program that takes a 3x3 matrix as input and prints the sum of each column.\n");
+
+            int[,] matrix = new int[3, 3];
+            int[] columnSum = { 0, 0, 0 };
+
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.Write($"For [{i}, {j}]: ");
+                    matrix[i-1, j-1] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("The final matrix is :");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < 3; j++)
+                {
+
+                                   
+                        columnSum[j] += matrix[i, j];
+                    
+
+                    Console.Write($" {matrix[i,j]} ");
+                   
+                }
+
+            }
+
+        Console.WriteLine("\n\n");
+            for (int j = 0; j < 3; j++)
+        {
+            Console.WriteLine($"Sum of column {j} = {columnSum[j]}");
+
+        }
+
     }
+    
 }
--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c00c072c8e7d39d3c98d2531f4fb2426e6db081")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
