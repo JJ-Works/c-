4c722acb00fd8bebc08b9d714c0407728160f846
Janarjan <janarjanrb@gmail.com>
2026-04-07 14:26:42 +0545

    new programs added.

---
+++ Lab1/Lab3-5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Lab1
+{
+    internal class Lab3_5
+    {
+        public void transpose()
+        {
+            Console.WriteLine("Write a C# program that transposes a given 3x3 matrix.");
+
+            int[,] original = new int[3, 3];
+            int[,] transposed = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                }
+}
+            }
+        }
+    }
+}
